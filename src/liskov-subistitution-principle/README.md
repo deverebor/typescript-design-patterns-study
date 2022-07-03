12d7ebffc16afab7012b59484e8d90d2bf8dd4d7
Lucas Souza <lucasp.sdev@gmail.com>
2022-07-03 14:21:04 -0300

    feat(lsp): liskov subistitution

---
+++ src/liskov-subistitution-principle/README.md
@@ -0,0 +1,3 @@
+# Liskov Subistitution Principle
+
+The liskov principle says that a subtype of a subclass must be replaced by the base class.
---
+++ src/liskov-subistitution-principle/classes/shopping-cart.ts
@@ -0,0 +1,39 @@
+import { ICartItem } from './interfaces'
+import { Discount } from './discount'
+
+export class ShoppingCart {
+	private readonly _items: ICartItem[] = []
+
+	constructor(private readonly discount: Discount) {}
+
+	get items(): Readonly<ICartItem[]> {
+		return this._items
+	}
+
+	addItem(newItem: ICartItem): void {
+		this._items.push(newItem)
+	}
+
+	removeItem(excludeItem: number): void {
+		this._items.splice(excludeItem, 1)
+	}
+
+	totalCart(): number {
+		return +this._items
+			.reduce((total, item) => total + item.price, 0)
+			.toFixed(2)
+	}
+
+	totalWithDescount() {
+		return +this.discount.applyDiscount(this.totalCart()).toFixed(2)
+	}
+
+	isEmptyCart(): boolean {
+		return this._items.length === 0
+	}
+
+	clear() {
+		console.log('Clearing cart...')
+		this._items.length = 0
+	}
+}
